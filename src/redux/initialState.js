7fe726ea44a3e64cdb592b543fe8fea8c0215cb0
libertalec <staskrutvk@mail.ru>
2024-01-11 06:00:17 +0300

    finish routing

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,5 +1,5 @@
+import { clone } from '@core/utils';
 import { defaultStyles, defaultTitle } from '@/constants';
-import { storage } from '@core/utils';
 
 const defaultState = {
     title: defaultTitle,
@@ -9,6 +9,7 @@ const defaultState = {
     stylesState: {},
     currentText: '',
     currentStyles: defaultStyles,
+    openedDate: new Date().toJSON(),
 };
 
 const normalize = state => ({
@@ -17,5 +18,6 @@ const normalize = state => ({
     currentText: '',
 });
 
-export const initialState = storage('excel-state') ?
-    normalize(storage('excel-state')) : defaultState;
+export function normalizeInitialState(state) {
+    return state ? normalize(state) : clone(defaultState);
+}
